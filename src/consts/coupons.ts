41f96307deaf2bae6ca995c5a4a0f5ea08199aca
Christian Duarte <w.christian.duarte@outlook.com>
2025-08-05 15:37:51 -0700

    New coupon const data

---
+++ src/consts/coupons.ts
@@ -0,0 +1,30 @@
+import type Coupon from "@/types/coupon";
+
+export const COUPONS: Coupon[] = [
+    {
+        layers: [
+            {
+                src: "/coupons/image1.png",
+                alt: "coupon"
+            }
+        ],
+        width: 160,
+        height: 90,
+        alt: "coupon"
+    },
+    {
+        layers: [
+            {
+                src: "/coupons/image2-bg.png",
+                alt: "coupon"
+            },
+            {
+                src: "/coupons/image2.png",
+                alt: "coupon"
+            }
+        ],
+        width: 160,
+        height: 90,
+        alt: "coupon"
+    }
+];
---
+++ src/types/coupon.ts
@@ -0,0 +1,9 @@
+export default interface Coupon {
+    layers: {
+        src: string;
+        alt: string;
+    }[];
+    width: number;
+    height: number;
+    alt: string;
+}
